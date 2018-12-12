1f0ee1f9baaf9d427311b9228b2e4d33299f92b1
Varnita Mathur <43050986+Varnita-Mathur@users.noreply.github.com>
2018-12-12 14:39:11 -0700

    Update ica00.c

--- ica00.c
+++ ica00.c
@@ -11,31 +11,31 @@ int main(int argc, char** argv)
 	//creating an array 
 	//type_name[size] = {set, of, initial, values}
 
-	int values[5] = { 1,2,3,4,5 };
-	printf("\%i", values[2]);
+	int values[5] = { 1,2,3,4,5 }; //array of 5 ints
+	printf("%i", values[2]); //print 3rd element
 	  
 
-	char Name[] = { 'B', 'o','b',0 };
+	char Name[] = { 'B', 'o','b',0 }; //char array Name and SameName are the same char array
 	char sameName[] = "Bob";
 
 
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //check mem leaks
 
 
 	printf("My very first C program! Well, at least for CNT.");
 	printf("\n\n");
 	fflush(stdout);
 
-	int *p = (int *)malloc(sizeof(int));
+	int *p = (int *)malloc(sizeof(int)); //int pointer malloc
 
-	*p = 10;
+	*p = 10; //points to value 10
 	printf("\nI'm trying to leak %d bytes of memory\n Anarchy now!\n", sizeof(*p));
 
 
 	system("pause");
 
 
-	system("pause");
+	
 
 
 	return 0;
